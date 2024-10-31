c6179372f191a8553ab45e94c7afb93cdd88a14b
krishna <krisch.webdev@gmail.com>
2024-10-31 16:36:11 +0100

    joiValidation-updated-and-inOrder

--- express-api/controllers/users.js
+++ express-api/controllers/users.js
@@ -1,5 +1,8 @@
 // express-api/controllers/users.js
 const User = require('../models/User');
+const createUserSchema = require('../schema/userSchema');
+
+
 
 // Retrieve all users
 exports.getAllUsers = async (req, res) => {
@@ -12,7 +15,7 @@ exports.getAllUsers = async (req, res) => {
 };
 
 // Create a new user
-exports.createUser = async (req, res) => {
+/*  exports.createUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const user = await User.create({ name, email, password });
@@ -20,7 +23,27 @@ exports.createUser = async (req, res) => {
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
+};  */
+
+exports.createUser = async (req, res) => {
+  try {
+    // Validate the request body with Joi
+    const { error, value } = createUserSchema.validate(req.body, { abortEarly: false });
+    
+    if (error) {
+      // Return all validation error details
+      return res.status(400).json({ error: error.details.map(detail => detail.message) });
+    }
+
+    // Proceed with creating the user if validation passed
+    const { name, email, password } = value;
+    const user = await User.create({ name, email, password });
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
+
 
 // Retrieve a specific user by ID
 exports.getUserById = async (req, res) => {
